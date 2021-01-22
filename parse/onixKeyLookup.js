9448e9765aa61886f32810f209ededbd491cea7b
Jando Gonzales <jandogonzales@pop-os.localdomain>
2021-01-22 11:27:49 -0700

    Added new types for the onix lookup, next parse the objects and place them in categories to create book objs

--- parse/onixKeyLookup.js
+++ parse/onixKeyLookup.js
@@ -2,7 +2,19 @@ module.exports = {
     // Key lookup for ONIX Elements to Types, Values, Etc in the XSD Schema 
     onixKeyLookup: {
         // This will be a map from Keys to Functions for each object type 
-        x: 1,
-        y: 2
+        notification: {},   // List 1 (notification types) 
+        publishingStatus: {}, // List 64 (publishing status) 
+        id: {}, // List 5 (product ids)
+        cityOfPublication: {},
+        publicationDate: {},
+        title: {},  // List 15 (title type) 
+        contributors: {},   // List 17 (contributor code ie author) 
+        subjects: {},   // List 26 (main subject id ie genres)
+        medias: {}, // List 38-40 (media file type, format, link type)
+        supply: {}, // List58 (prices and type codes)
+        texts: {},  // List 33 (Text type code ie main summary)
+        imprint: {},   // Publisher name??
+        publisher: {},  // Publisher name and role (seems more legit)
+        salesRights: {} // List 46 (sales rights type code)
     } 
 }
